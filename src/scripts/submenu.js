d4d76c3ee68aaa52a2c377578fd8053114dcae71
imonaar <kevinmonari22@gmail.com>
2021-09-10 17:49:55 +0300

    progress

--- src/scripts/submenu.js
+++ src/scripts/submenu.js
@@ -1,17 +1,10 @@
 const menuItems = Array.from(document.querySelectorAll('li.js-has-submenu'));
 
-// console.log(menuItems)
-// let timer;
-// menuItems.forEach((item, index)=>{
-//   item.addEventListener('mouseover',(event)=>{
-//     console.log('in')
-//     document.querySelector('.submenu').className = '.submenu .open'
-//     clearTimeout(timer)
-//   })
+const hamburger = document.querySelector('.js-hamburger')
+const navMobile = document.querySelector('.js-nav-mobile')
 
-//   item.addEventListener('mouseout', (event)=>{
-//     timer = setTimeout((event) => {
-//       item.querySelector('.submenu.open').className = ('.submenu')
-//     }, 1000);
-//   })
-// })
+hamburger.addEventListener('click', ()=>{
+  navMobile.classList.toggle('open')
+  hamburger.classList.toggle('close')
+})
+
