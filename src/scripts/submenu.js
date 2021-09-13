43d95ed1398a8f0ee5acb120738cf853f29deea2
imonaar <kevinmonari22@gmail.com>
2021-09-13 21:19:17 +0300

    final

--- src/scripts/submenu.js
+++ src/scripts/submenu.js
@@ -1,4 +1,3 @@
-const menuItems = Array.from(document.querySelectorAll('li.js-has-submenu'));
 
 const hamburger = document.querySelector('.js-hamburger')
 const navMobile = document.querySelector('.js-nav-mobile')
